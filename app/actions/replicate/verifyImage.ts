299381b8475772b2f3917db77ec95170f5d8b0a0
A Mohamed Aakhil <a.aakhilmohamed@gmail.com>
2024-01-05 10:43:46 +0530

    fix prompt

--- app/actions/replicate/verifyImage.ts
+++ app/actions/replicate/verifyImage.ts
@@ -9,7 +9,7 @@ export async function verifyImage({answer, email}: {answer: string, email: strin
     await dbConnect();
     const user = await User.findOne({ email:email });
     const completion = await openai.chat.completions.create({
-        messages: [{ role: "system", content: "You are supposed to say only yes or no. Provided in the first line will be a statement, and in the second line will be another statement. You have to say if they mean the same thing with a yes or no." },
+        messages: [{ role: "system", content: "You are supposed to say only yes or no. Provided in the first line will be a statement, and in the second line will be another statement. You have to say if the statements mean the same thing, they do not have to be exactly the same." },
         { role: "user", content: "Statement 1: " + answer + "\n" + "Statement 2: " + user.question },
     ],
         model: "gpt-3.5-turbo",
