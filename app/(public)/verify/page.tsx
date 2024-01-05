144278d254c4ca498bbd419e1a3dcc6925e00b5c
A Mohamed Aakhil <a.aakhilmohamed@gmail.com>
2024-01-05 09:08:27 +0530

    feat: image generation

---
+++ app/(public)/verify/page.tsx
@@ -0,0 +1,24 @@
+import User from "@/app/models/User";
+import { authOptions } from "@/app/utils/auth";
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
+import { FC } from "react";
+import { generateImage } from "@/app/actions/replicate/generateImage";
+import Image from "next/image";
+interface pageProps {}
+
+const page: FC<pageProps> = async ({}) => {
+  
+  const session = await getServerSession(authOptions);
+  if (!session) {
+      redirect("/login/?callbackUrl=/verify");
+  } 
+
+  const image = await generateImage("");
+  console.log(image);
+  return <div className='items-center flex h-screen'>
+    <Image src={image[0]} width={500} height={500} alt="prompt" />
+  </div>
+};
+
+export default page;
